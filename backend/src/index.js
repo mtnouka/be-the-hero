d0b1bb21ee1e3abb25ec5e4af246ee61db42ce49
mtnouka <mtheus.csouza@gmail.com>
2020-03-28 11:30:36 -0300

    feat: criação de validação de rotas

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,6 +26,7 @@
    
 const express = require('express');
 const cors = require('cors');
+const { errors } = require('celebrate');
 const routes = require('./routes');
 
 const app = express();
@@ -33,5 +34,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
 app.listen(3333);
